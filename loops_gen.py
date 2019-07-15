9700f16b2386c21914cf50d11fa0c46274972814
MarynaSavchenko <marinavadimovna97@gmail.com>
2019-07-15 11:49:46 +0200

    Change ranges. Add MAX_DEPTH

--- loops_gen.py
+++ loops_gen.py
@@ -9,10 +9,12 @@ import numpy as np
 MAX_NUMBER_OF_ARRAY = 5
 MAX_DIM_SIZE = 2 ** 15
 MAX_DIMS = 3
+MAX_DEPTH = 3
 
 maths_operations = ['+', '-', '*', '/']
 array_init_functions = {1: 'create_one_dim', 2: 'create_two_dim', 3: 'create_three_dim'}
 dim_size = random.randint(1, MAX_DIM_SIZE)
+loop_nest_depth = random.randint(1, MAX_DEPTH)
 
 
 def generate_nested_loops(loop_nest_depth, num_of_iters):
@@ -41,7 +43,6 @@ def print_loop_structure(loop_index, lower_bound, upper_bound, fun):
 
 def create_nested_loop():
     """calls generate_nested_loops(d, i) and write it to file"""
-    loop_nest_depth = random.randint(1, 5)
     num_of_iters = random.randint(1, 10)
     with open('src/feature1.c', 'a+') as file:
         file.write('\n\n')
@@ -57,7 +58,7 @@ def generate_loop_index(loop_level):
 
 def generate_array_index(array_id):  # up to 26 letters
     first_iterator = 'A'
-    calculated_index = chr(ord(first_iterator) + array_id)
+    calculated_index = chr(ord(first_iterator) + array_id - 1)
     return calculated_index
 
 
@@ -74,7 +75,7 @@ def init_arrays():
     dict_of_arrays = {}
     for i in range(number_of_arrays):
         dim = generate_array_dimensions()
-        index = generate_array_index(i)
+        index = generate_array_index(i + 1)
         write_array_to_file(index, dim)
         dict_of_arrays[index] = dim
     return dict_of_arrays
@@ -100,22 +101,22 @@ def generate_array_dimensions():
 
 
 def generate_calculations(arrays_dict):  # todo randomize the parameters
-    num_of_calculations = random.randint(1, 16)
+    num_of_calculations = random.randint(0, 14)
     calculations = ""
-    for i in range(1, num_of_calculations):
+    for i in range(num_of_calculations + 2):
         coin_flip = random.randint(0, 1)
         if coin_flip == 0:
             array_index, array_dims = random.choice(list(arrays_dict.items()))
             number_of_dims = len(array_dims)
             calculations += array_index
             for j in range(number_of_dims):
-                rand_parameter = random.randint(1, len(array_dims))
-                array_id = generate_loop_index(rand_parameter)
+                rand_parameter = random.randint(0, loop_nest_depth - 1)
+                array_id = generate_loop_index(rand_parameter+1)
                 calculations += '[' + str(array_id) + ']'  # random array value
         else:
             calculations += str(random.random())  # random scalar
 
-        if not (i == num_of_calculations - 1):
+        if not (i == num_of_calculations + 1):
             calculations += random.choice(maths_operations)  # random operator
     return calculations
 
