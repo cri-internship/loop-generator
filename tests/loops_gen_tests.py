8f3945080979a21ebbfdd53713ba70acea3a9ae9
MarynaSavchenko <marinavadimovna97@gmail.com>
2019-07-16 15:31:44 +0200

    True tests

--- tests/loops_gen_tests.py
+++ tests/loops_gen_tests.py
@@ -19,7 +19,7 @@ class TestPrint_loop_structure(unittest.TestCase):
 
 class TestGenerate_loop_index(unittest.TestCase):
     def test_index_26(self):
-        self.assertEqual(lg.generate_loop_index(26), 'b')
+        self.assertEqual(lg.generate_loop_index(26), 'a')
 
     def test_index_0(self):
         self.assertEqual(lg.generate_loop_index(0), 'a')
