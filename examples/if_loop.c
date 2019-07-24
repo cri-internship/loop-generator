42db2d5a4501ae17fba7ce0c8971f478cac49d70
MarynaSavchenko <marinavadimovna97@gmail.com>
2019-07-24 14:44:55 +0200

    Ready to test

--- examples/if_loop.c
+++ examples/if_loop.c
@@ -1,11 +1,9 @@
 int main() {
 
 for(int j=0; j<M; ++j) {
-c[i][j] = a[j][i] * c[o]+ u[o][p];
+c[i][j] = a[j][i] * c[o] + b[u] *6 *z;
     for(int i=0; i<N; ++i) {
         c[i][j] = a[j][i] * c[o];
-        c[i][j] = a[j][i] * c[o]+ u[o][p];
-
     }
 }
 
--- loopVisitor.py
+++ loopVisitor.py
@@ -5,7 +5,7 @@ class MyVisitor(c_ast.NodeVisitor):
     def __init__(self):
         self.features_dict = {'loop depth': 0, 'unique arrays read': set(), 'unique arrays write': set(),
                               'statements per loop level': {}, 'unique arrays read per statement': {},
-                              'dimensions per array read': 0, }
+                              'dimensions per array read': {}, }
         self.loop_lvl = 0
         self.stmt_num = 0
 
@@ -18,14 +18,39 @@ class MyVisitor(c_ast.NodeVisitor):
     def visit_Assignment(self, node):
         self.features_dict['statements per loop level'][self.loop_lvl - 1] += 1
         self.features_dict['unique arrays write'].add(count_arrays_write(node.lvalue))
-        count_arrays_read(node.rvalue, self.features_dict['unique arrays read'])
-        self.features_dict['unique arrays read per statement'][self.stmt_num] = set()
-        count_arrays_read(node.rvalue, self.features_dict['unique arrays read per statement'][self.stmt_num])
+
+        tmp = {}
+        count_arrays_read(node.rvalue, tmp)
+
+        self.features_dict['dimensions per array read'].update(tmp)
+
+        self.features_dict['unique arrays read per statement'][self.stmt_num] = len(tmp)
+
+        self.features_dict['unique arrays read'].update(tmp.keys())
+
         self.stmt_num += 1
 
     def print_features(self):
         for key, value in self.features_dict.items():
-            print(f'{key}:{value}')
+            if type(value) == set:
+                print(f'{key}:{len(value)}')
+            else:
+                print(f'{key}:{value}')
+
+
+def count_arrays_read(node, array_dims):
+    try:
+        if type(node.left) != c_ast.Constant and type(node.left) != c_ast.ID:
+            count_arrays_read(node.left, array_dims)
+        if type(node.right) != c_ast.Constant and type(node.right) != c_ast.ID:
+            count_arrays_read(node.right, array_dims)
+    except AttributeError:
+        counter = 1
+        while type(node.name) != c_ast.ID:
+            node = node.name
+            counter += 1
+
+        array_dims[node.name.name] = counter
 
 
 def count_arrays_write(node):
@@ -33,18 +58,3 @@ def count_arrays_write(node):
         return f'{node.name.name}'
     if type(node.name) != c_ast.ID:
         return count_arrays_write(node.name)
-
-
-def count_arrays_read(node, tmp):
-    try:
-        if type(node.left) != c_ast.Constant:
-            count_arrays_read(node.left, tmp)
-        if type(node.right) != c_ast.Constant:
-            count_arrays_read(node.right, tmp)
-    except AttributeError:
-        while type(node.name) != c_ast.ID:
-            node = node.name
-        tmp.add(node.name.name)
-
-
-
