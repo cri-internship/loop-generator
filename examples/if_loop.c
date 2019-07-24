f0c728ab82aad110648557da81da845686763198
MarynaSavchenko <marinavadimovna97@gmail.com>
2019-07-24 11:40:04 +0200

    Unique arrays read per statement. Count from 0

--- examples/if_loop.c
+++ examples/if_loop.c
@@ -1,7 +1,10 @@
 int main() {
 
 for(int j=0; j<M; ++j) {
+c[i][j] = a[j][i] * 8 *k[i] - 45 + k[o];
     for(int i=0; i<N; ++i) {
+        c[i][j] = a[j][i] * 8 *b[i] - 45 + k[o];
+        c[i][j] = a[j][i] * 8 *k[i] - 45 + k[o];
         c[i][j] = a[j][i] * 8 *b[i] - 45 + k[o];
     }
 }
--- loopVisitor.py
+++ loopVisitor.py
@@ -4,20 +4,23 @@ from pycparser import c_ast
 class MyVisitor(c_ast.NodeVisitor):
     def __init__(self):
         self.features_dict = {'loop depth': 0, 'unique arrays read': set(), 'unique arrays write': set(),
-                              'statements per loop level': {}, 'unique arrays read per statement': 0,
+                              'statements per loop level': {}, 'unique arrays read per statement': {},
                               'dimensions per array read': 0, }
         self.loop_lvl = 0
+        self.stmt_num = 0
 
     def visit_For(self, node):
         self.features_dict['loop depth'] += 1
-        self.loop_lvl += 1
         self.features_dict['statements per loop level'][self.loop_lvl] = 0
+        self.loop_lvl += 1
         self.generic_visit(node)
 
     def visit_Assignment(self, node):
-        self.features_dict['statements per loop level'][self.loop_lvl] += 1
+        self.features_dict['statements per loop level'][self.loop_lvl - 1] += 1
         self.features_dict['unique arrays write'].add(count_arrays_write(node.lvalue))
         self.features_dict['unique arrays read'].update(count_arrays_read(node.rvalue))
+        self.features_dict['unique arrays read per statement'][self.stmt_num] = count_arrays_read(node.rvalue)
+        self.stmt_num += 1
 
     def print_features(self):
         for key, value in self.features_dict.items():
