9fb6fefa34545c336df264c65900b7c8a34ac8cb
MarynaSavchenko <marinavadimovna97@gmail.com>
2019-07-25 11:16:41 +0200

    Get rid of self.current_stmt_num

--- loopVisitor.py
+++ loopVisitor.py
@@ -3,10 +3,13 @@ from pycparser import c_ast
 
 class MyVisitor(c_ast.NodeVisitor):
     def __init__(self):
+        """attributes: features_dict: dict with necessary features and initial values
+                     current_parent: every node, except the first one has parent node
+        """
         self.features_dict = {'loop depth': 0, 'unique arrays read': set(), 'unique arrays write': set(),
                               'statements per loop level': {}, 'unique arrays read per statement': {},
                               'dimensions per array read': {}, }
-        self.current_stmt_num = 0
+
         self.current_parent = None
 
     def visit_For(self, node):
@@ -18,7 +21,6 @@ class MyVisitor(c_ast.NodeVisitor):
         self.generic_visit(node)
 
     def visit_Assignment(self, node):
-
         if type(self.current_parent) == c_ast.Compound or self.current_parent.stmt == node:
             current_loop_lvl = self.features_dict['loop depth'] - 1
             self.features_dict['statements per loop level'][current_loop_lvl] += 1
@@ -30,11 +32,13 @@ class MyVisitor(c_ast.NodeVisitor):
             count_arrays_read(node.rvalue, tmp)
 
             self.features_dict['dimensions per array read'].update(tmp)
-
-            self.features_dict['unique arrays read per statement'][self.current_stmt_num] = len(tmp)
+            try:
+                current_stmt_num = list(self.features_dict['unique arrays read per statement'].keys())[-1] + 1
+            except IndexError:
+                current_stmt_num = 0
+            self.features_dict['unique arrays read per statement'][current_stmt_num] = len(tmp)
 
             self.features_dict['unique arrays read'].update(tmp.keys())
-            self.current_stmt_num += 1
 
     def print_features(self):
         for key, value in self.features_dict.items():
@@ -68,7 +72,6 @@ def count_arrays_read(node, array_dims):
             while type(node.name) != c_ast.ID:
                 node = node.name
                 counter += 1
-
             array_dims[node.name.name] = counter
 
 
