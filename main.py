ec8d17271cc5d3ef3a295485e6a2cd7b89673544
Justyna Zawalska <justynajzawalska@gmail.com>
2019-08-07 16:31:12 +0200

    Add part of stmts generated randomly

--- main.py
+++ main.py
@@ -4,41 +4,41 @@ import loops_gen as lg
 # check if sizes are the same
 import random
 
-unique_arrays_write = {}
-unique_arrays_read = {}
+unique_arrays_write = {"used": [], "unused": []}
+unique_arrays_read = {"used": [], "unused": []}
 
 
 def flow_dependency(array_name):
     result = f'{array_name}={do_random(unique_arrays_read)}\n' \
-             f'{gen_random_stmt_write()}={array_name}'
+        f'{gen_random_stmt(unique_arrays_write)}={array_name}'
     return result
 
 
 def anti_dependency(array_name):
-    result = f'{gen_random_stmt_write()}={array_name}\n' \
-             f'{array_name}={do_random(unique_arrays_read)}'
+    result = f'{gen_random_stmt(unique_arrays_write)}={array_name}\n' \
+        f'{array_name}={do_random(unique_arrays_read)}'
     return result
 
 
 def output_dependency(array_name):
     result = f'{array_name}={do_random(unique_arrays_read)}\n' \
-             f'{array_name}={do_random(unique_arrays_read)}'
+        f'{array_name}={do_random(unique_arrays_read)}'
     return result
 
 
 def input_dependency(array_name):
-    result = f'{gen_random_stmt_write()}={array_name}\n' \
-             f'{gen_random_stmt_write()}={array_name}'
+    result = f'{gen_random_stmt(unique_arrays_write)}={array_name}\n' \
+        f'{gen_random_stmt(unique_arrays_write)}={array_name}'
     return result
 
 
-def gen_random_stmt_write():
-    if unique_arrays_write['unused']:
-        el = random.choice(list(unique_arrays_write['unused']))
-        unique_arrays_write['unused'].remove(el)
-        unique_arrays_write['used'].append(el)
+def gen_random_stmt(unique_arrays):
+    if unique_arrays['unused']:
+        el = random.choice(list(unique_arrays['unused']))
+        unique_arrays['unused'].remove(el)
+        unique_arrays['used'].append(el)
     else:
-        el = random.choice(list(unique_arrays_write['used']))
+        el = random.choice(list(unique_arrays['used']))
     el = parse_array(el)
     str = el[0]
     for size in range(len(el[1])):
@@ -46,20 +46,27 @@ def gen_random_stmt_write():
     return str
 
 
-
 def parse_array(name_with_dims):
     name_with_dims = name_with_dims.split('[')
     array_name = name_with_dims[0]
     sizes = name_with_dims[1][:-1].split(',')
     sizes = list(map(int, sizes))
-    return (array_name, sizes)
+    return [array_name, sizes]
 
 
-def do_random(arg):
-    pass
+def do_random(read_array):
+    maths_operations = ['+', '-', '*', '/']
+    num_of_calculations = random.randint(2, 6)
+    stmt = ""
+    for i in range(num_of_calculations):
+        stmt += gen_random_stmt(read_array)
+        stmt += random.choice(maths_operations)
+
+    stmt += str(random.random())  # random scalar
+    return stmt[:-1]
 
 
-def parse_input():
+def parse_input_old():
     with open('input.json', 'r') as file:
         data = json.load(file)
         data = data[0]
@@ -72,18 +79,38 @@ def parse_input():
         dependencies = data['dependencies']
 
 
+def parse_input():
+    with open('input.json', 'r') as file:
+        data = json.load(file)
+        data = data[0]
+        global loop_nest_level, unique_arrays_write, unique_arrays_read, dependencies
+        loop_nest_level = data['loop_nest_level']
+        unparsed_arrays_write = data['unique_arrays_write']
+        unparsed_arrays_read = data['unique_arrays_read']
+        for arr in unparsed_arrays_write:
+            unique_arrays_write['unused'].append(arr)
+
+        for arr in unparsed_arrays_read:
+            unique_arrays_read['unused'].append(arr)
+
+        unique_arrays_write['used'] = []
+        unique_arrays_read['used'] = []
+        dependencies = data['dependencies']
+
+
 if __name__ == '__main__':
     parse_input()
+    print(do_random(unique_arrays_read))
     # print(loop_nest_level)
     # print(unique_arrays_write)
     # print(unique_arrays_read)
     # print(dependencies)
     # print(unique_arrays_write)
     # print(unique_arrays_read)
-    print(input_dependency("A[i]"))
+    # print(input_dependency("A[i]"))
     # print(unique_arrays_write)
     # print(unique_arrays_read)
-    print(flow_dependency("A[i]"))
+    # print(flow_dependency("A[i]"))
     # print(unique_arrays_write)
     # print(unique_arrays_read)
     # print(lg.generate_calculations(unique_arrays_read))
