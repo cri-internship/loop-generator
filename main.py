608ebc799254248df7159d9b4c7d552ff396bbfc
MarynaSavchenko <marinavadimovna97@gmail.com>
2019-08-07 14:25:19 +0200

    Parse array name

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+import loops_gen as lg
 
 # check if sizes are the same
 import random
@@ -9,12 +10,12 @@ unique_arrays_read = {}
 
 def flow_dependency(array_name):
     result = f'{array_name}={do_random(unique_arrays_read)}\n' \
-             f'{do_random(unique_arrays_write)}={array_name}'
+             f'{gen_random_stmt_write()}={array_name}'
     return result
 
 
 def anti_dependency(array_name):
-    result = f'{do_random(unique_arrays_write)}={array_name}\n' \
+    result = f'{gen_random_stmt_write()}={array_name}\n' \
              f'{array_name}={do_random(unique_arrays_read)}'
     return result
 
@@ -26,19 +27,31 @@ def output_dependency(array_name):
 
 
 def input_dependency(array_name):
-    result = f'{do_random(unique_arrays_write)}={array_name}\n' \
-             f'{do_random(unique_arrays_write)}={array_name}'
+    result = f'{gen_random_stmt_write()}={array_name}\n' \
+             f'{gen_random_stmt_write()}={array_name}'
     return result
 
 
-def do_random(write_or_read):
-    if write_or_read['unused']:
-        el = random.choice(list(write_or_read['unused']))
-        write_or_read['unused'].remove(el)
-        write_or_read['used'].append(el)
-        return el
+def gen_random_stmt_write():
+    if unique_arrays_write['unused']:
+        el = random.choice(list(unique_arrays_write['unused']))
+        unique_arrays_write['unused'].remove(el)
+        unique_arrays_write['used'].append(el)
     else:
-        return random.choice(list(write_or_read['used']))
+        el = random.choice(list(unique_arrays_write['used']))
+    return parse_array(el)
+
+
+def parse_array(name_with_dims):
+    name_with_dims = name_with_dims.split('[')
+    array_name = name_with_dims[0]
+    sizes = name_with_dims[1][:-1].split(',')
+    sizes = list(map(int, sizes))
+    return (array_name, sizes)
+
+
+def do_random(arg):
+    pass
 
 
 def parse_input():
@@ -57,15 +70,15 @@ def parse_input():
 if __name__ == '__main__':
     parse_input()
     # print(loop_nest_level)
-    print(unique_arrays_write)
-    print(unique_arrays_read)
+    # print(unique_arrays_write)
+    # print(unique_arrays_read)
     # print(dependencies)
     # print(unique_arrays_write)
     # print(unique_arrays_read)
     print(input_dependency("A[i]"))
-    print(unique_arrays_write)
-    print(unique_arrays_read)
+    # print(unique_arrays_write)
+    # print(unique_arrays_read)
     print(flow_dependency("A[i]"))
-    print(unique_arrays_write)
-    print(unique_arrays_read)
+    # print(unique_arrays_write)
+    # print(unique_arrays_read)
     # print(lg.generate_calculations(unique_arrays_read))
