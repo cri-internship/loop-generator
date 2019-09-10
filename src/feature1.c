0d7c5c4eb52cd4f1ad16ad139feec42ca6fdf6b0
Justyna Zawalska <justynajzawalska@gmail.com>
2019-09-10 10:50:53 +0200

    Update dependencies generation

--- src/feature1.c
+++ src/feature1.c
@@ -9,37 +9,26 @@ int main( int argc, const char* argv[] )
     srand(time(NULL));
 
     
-	float **B = create_two_dim_float(50, 50, "ones");
-	float *A = create_one_dim_float(100, "ones");
-	float ***C = create_three_dim_float(50, 50, 50, "ones");
+	float **B = create_two_dim_float(50, 50);
+	float *A = create_one_dim_float(100);
+	float ***C = create_three_dim_float(50, 50, 50);
 
 	for (int c = 0; c < 50; c++)
-	  for (int b = 0; b < 45; b++)
-	    for (int a = 1; a < 48; a++)
+	  for (int b = 0; b < 50; b++)
+	    for (int a = 0; a < 50; a++)
 	    {
 	      
-	C[a][b][c]=0.25401;
 	      
-	float  var_a=C[a][b][c]*0.14561;
+	      C[a][b][c]=0.71372;
 	      
-	      A[a]=A[a-1]*0.70073;
+	      float  var_a=C[a][b][c]*0.85023;
 	      
-	      float var_b=C[a][b][c]-0.45714;
-	      C[a][b][c]=0.88477;
 	      
-	      A[a]=A[a+1]-0.15982;
+	float var_b=C[a][b][c]*0.61561;
 	      
-	      C[a][b][c]=0.75868;
-	      C[a][b][c]=0.04778;
-	      
-	      B[a][b+2]=0.50736;
-	      B[a][b+2]=0.93726;
-	      
-	      float var_c=C[a][b][c]/0.35187;
-	      float var_d=C[a][b][c]/0.94293;
-	      
-	      float var_e=A[a-1]+0.18177;
-	      float var_f=A[a-1]*0.70171;
+	C[a][b][c]=0.90687;
+	      ;
+	      ;
 	    }
 
     return 0;
