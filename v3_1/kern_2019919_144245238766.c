da99dee0c5bfb3f5c569f0b5e71047d423afcf9a
MarynaSavchenko <mvadimovna@ukr.net>
2019-09-19 15:13:48 +0200

    Add codes to v3_1

---
+++ v3_1/kern_2019919_144245238766.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "../init_array_lib/init_dyn_array.h"
+
+
+int main( int argc, const char* argv[] )
+{
+    srand(time(NULL));
+
+    
+	float **B = create_two_dim_float(50, 50, "random");
+	float *A = create_one_dim_float(100, "random");
+	float ***C = create_three_dim_float(40, 150, 80, "random");
+
+	for (int c = 13; c < 50; c++)
+	  for (int b = 3; b < 50; b++)
+	    for (int a = 3; a < 50; a++)
+	    {
+	      
+	      B[a][b-2]=B[a][b-13]-B[a-1][b-3]-0.941;
+	      
+	      B[a-3][b-2]=B[a-3][b-4]*0.795;
+	    }
+
+    return 0;
+}
---
+++ v3_1/kern_2019919_144749322453.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "../init_array_lib/init_dyn_array.h"
+
+
+int main( int argc, const char* argv[] )
+{
+    srand(time(NULL));
+
+    
+	float *A = create_one_dim_float(100, "random");
+	float **B = create_two_dim_float(50, 50, "random");
+	float ***C = create_three_dim_float(40, 150, 80, "random");
+
+	for (int c = 52; c < 50; c++)
+	  for (int b = 3; b < 50; b++)
+	    for (int a = 3; a < 50; a++)
+	    {
+	      
+	      B[a][b-2]=B[a][b-52]+B[a-1][b-3]/0.092;
+	      
+	      B[a-3][b-2]=B[a-3][b-4]*0.837;
+	    }
+
+    return 0;
+}
